b238ff242b61b3d1832a97868a890d9c12dea626
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-04-24 11:01:44 +0200

    Corrección Order model + relaciones index.js

--- models/index.js
+++ models/index.js
@@ -1,37 +1,11 @@
-'use strict';
+import { User } from "./user.js";
+import { Order } from "./order.js";
+import { Genre } from "./genre.js";
+import { Movie } from "./movie.js";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+//Relaciones 1:N
+User.hasMany(Order, { foreignKey: "userID" })
+Genre.hasMany(Movie, { foreignKey: "genreID" })
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+//Relaciones 1:1
+Movie.belongsTo(Order, { foreignKey: "movieID" })
--- models/order.js
+++ models/order.js
@@ -1,25 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Order.init({
-    rentstart: DataTypes.DATE,
-    rentend: DataTypes.DATE,
-    status: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
-  return Order;
-};
+
+import pkg from 'sequelize';
+import database from "../database.js"
+
+const { Model, DataTypes } = pkg;
+
+
+export class Order extends Model { }
+
+Order.init({
+  rentstart: DataTypes.DATE,
+  rentend: DataTypes.DATE,
+  status: DataTypes.STRING,
+  userID: DataTypes.INTEGER,
+  movieID: DataTypes.INTEGER
+}, {
+  sequelize: database,
+  modelName: 'Order',
+});
