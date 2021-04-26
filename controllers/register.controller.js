2283c53040152ddb5dcc48721af227fb5043cc7e
biorubenfs <biorubenfs@hotmail.com>
2021-04-26 23:35:57 +0200

    checkUser middleware now verifies email and password.

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -5,29 +5,34 @@ import bcrypt from "bcrypt";
 export const registerController = {
     create: async (req, res) => {
 
-        let email = req.body.email;
-        let password = req.body.password;
-        let hashTemp = 10;
+        try {
+            let email = req.body.email;
+            let password = req.body.password;
+            let hashTemp = 10;
 
-        const checkUser = await User.findOne({ where: { email: email } });
+            const checkUser = await User.findOne({ where: { email: email } });
 
-        const emailValidation = email.split("@");
+            const emailValidation = email.split("@");
 
-        // basic email validation
-        if (!checkUser && emailValidation.length === 2 && password.length > 3) {
+            // basic email validation
+            if (!checkUser && emailValidation.length === 2 && password.length > 3) {
 
-            try {
-                const passwordHash = await bcrypt.hash(password, hashTemp);
+                try {
+                    const passwordHash = await bcrypt.hash(password, hashTemp);
 
-                const newUser = await database.query(`INSERT INTO Users (email, password) VALUES ('${email}', '${passwordHash}')`)
-                console.log(`User with email ${email} was created`);
-                res.sendStatus(200);
+                    const newUser = await database.query(`INSERT INTO Users (email, password) VALUES ('${email}', '${passwordHash}')`)
+                    console.log(`User with email ${email} was created`);
+                    res.sendStatus(200);
 
-            } catch (error) {
-                res.status(400).send({ message: error.message });
+                } catch (error) {
+                    res.status(400).send({ message: error.message });
+                }
+            } else {
+                res.status(409).send({ message: "Email is already registered" });
             }
-        } else {
-            res.status(400).send({ message: "Email or password is invalid" });
+        } catch (error) {
+            res.status(400).send({ message: error.message })
         }
+
     }
 }
--- middlewares/checkUser.js
+++ middlewares/checkUser.js
@@ -1,10 +1,20 @@
 import jwt from 'jsonwebtoken';
+import { User } from '../models/index.js';
+import bcrypt from 'bcrypt';
 
 const checkUser = async (req, res, next) => {
     try {
         console.log("Verifying user...");
 
         jwt.verify(req.headers.token, process.env.SECRET);
+
+        const user = jwt.decode(req.headers.token);
+        const result = await User.findOne({ where: { email: user.email } })
+
+        if (!bcrypt.compareSync(req.headers.password, result.password)) {
+            return res.status(403).send({ message: "Email or password incorrect" })
+        }
+
         next();
 
     } catch (error) {
