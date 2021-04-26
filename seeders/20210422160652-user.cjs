90647781638ec509f05b66490ce767acbe99fd62
biorubenfs <biorubenfs@hotmail.com>
2021-04-26 19:42:26 +0200

    Change in seed user using hashed password

--- seeders/20210422160652-user.cjs
+++ seeders/20210422160652-user.cjs
@@ -1,22 +1,31 @@
 'use strict';
+const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    const hashTemp = 10;
+    const commonPassword = "1234";
+    const adminPassword = "admin";
+
+    const passwordHashUser = await bcrypt.hash(commonPassword, hashTemp);
+    const passwordHashAdmin = await bcrypt.hash(adminPassword, hashTemp);
+
     await queryInterface.bulkInsert('Users', [
       {
+        email: 'admin@gmail.com',
+        password: passwordHashAdmin,
+        role: 'admin'
+      },
+      {
         email: 'adri@gmail.com',
-        password: '1234',
+        password: passwordHashUser,
         role: 'user'
       },
       {
         email: 'ruben@gmail.com',
-        password: '1234',
+        password: passwordHashUser,
         role: 'user'
-      },
-      {
-        email: 'admin@gmail.com',
-        password: '1234',
-        role: 'admin'
       }
     ])
   },
